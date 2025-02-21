fadde92020e0a1520947fc4ad947615094eedfc6
Bryan Keller <bkeller@netflix.com>
2025-02-20 18:02:26 -0800

    use iceberg lib

--- src/main/java/org/bk/misc/ReflectionTest.java
+++ src/main/java/org/bk/misc/ReflectionTest.java
@@ -1,10 +1,13 @@
 package org.bk.misc;
 
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.bk.misc.json.JsonUtil;
+import org.apache.iceberg.util.JsonUtil;
 
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ReflectionTest {
 
@@ -14,6 +17,13 @@ public class ReflectionTest {
     ObjectMapper mapper = (ObjectMapper)field.get(null);
     mapper.getFactory().configure(JsonFactory.Feature.FAIL_ON_SYMBOL_HASH_OVERFLOW, false);
 
-    JsonUtil.parse();
+    String json = Files.readString(Paths.get("test.json"));
+    String name = JsonUtil.parse(json, ReflectionTest::fromJson);
+    System.out.println(name);
   }
+
+  public static String fromJson(JsonNode jsonNode) {
+    return jsonNode.get("name").asText();
+  }
+
 }
